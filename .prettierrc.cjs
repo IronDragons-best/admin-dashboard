80491bbddaad891055b43183a2dc6613c86f83ca
Vladimir Vasyliev <wvladimirw0502@gmail.com>
2025-09-15 20:03:56 +0300

    Initial commit with Apollo project setup

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  ...require('@it-incubator/prettier-config'),
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  endOfLine: 'auto',
+};
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ['storage.yandexcloud.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.yandexcloud.net',
+        pathname: '**',
+      },
+    ],
+  },
+  webpack: config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '.'), // Базовый алиас для корня
+    };
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      type: 'asset/resource',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
